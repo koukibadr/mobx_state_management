86ecdada1138535de897f114664537aeab4039df
Kouki Badr <badreddinekouki1998@gmail.com>
2022-03-03 07:53:18 +0100

    add mobx state class to home screen counter page

--- lib/counter_store.dart
+++ lib/counter_store.dart
@@ -2,7 +2,7 @@ import 'package:mobx/mobx.dart';
 
 part 'counter_store.g.dart';
 
-class Counter = _Counter with _$Counter;
+class CounterStore = _Counter with _$Counter;
 
 abstract class _Counter with Store {
 
